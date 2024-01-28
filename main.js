f4efcfe44b4a0f0f5916ad16d1d8b2055f406aeb
cmcnamee25 <cmcnamee4@gmail.com>
2024-01-28 00:48:56 +0000

    Added Previous and Next tasks to spreadsheet

--- main.js
+++ main.js
@@ -8,7 +8,9 @@ const csvWriter = createCsvWriter({
         {id: 'type', title: 'Type'},
         {id: 'objectives', title: 'Objectives'},
         {id: 'rewards', title: 'Rewards'},
-        {id: 'kappa', title: 'Required for Kappa'}
+        {id: 'kappa', title: 'Required for Kappa'},
+        {id: 'prev', title: 'Previous Quest'},
+        {id: 'next', title: 'next'},
     ]
 });
 
@@ -18,7 +20,7 @@ const csvWriter = createCsvWriter({
 
     await page.goto("https://escapefromtarkov.fandom.com/wiki/Quests");
 
-    const traderNames = ['Prapor', 'Therapist', 'Skier', 'Peacekeeper', 'Mechanic', 'Ragman', 'Jaeger', 'Fence'];
+    const traderNames = ['Prapor', 'Therapist', 'Skier', 'Peacekeeper', 'Mechanic', 'Fence', 'Ragman', 'Jaeger', 'Lightkeeper'];
     let allQuests = [];
 
     for(let name of traderNames) {
@@ -28,25 +30,79 @@ const csvWriter = createCsvWriter({
         const tableExists = await page.$(selector) !== null;
         
         console.log(selector, tableExists);
-        const quests = await page.$$eval(`${selector} tr`, rows => {
+        const quests = await page.$$eval(`${selector} tbody tr`, rows => {
             return rows.map(row => {
                 const ths = row.querySelectorAll('th');
                 const tds = row.querySelectorAll('td');
-                
+                const link = row.querySelector('a');
+                const link_href = link.getAttribute('href');
+
                 return {
                     title: ths[0]?.innerText.trim(),
                     type: ths[1]?.innerText.trim(),
                     objectives: tds[0]?.innerText.trim(),
                     rewards: tds[1]?.innerText.trim(),
-                    kappa: ths[2]?.innerText.trim()
+                    kappa: ths[2]?.innerText.trim(),
+                    link: link_href
                 }
             })
         });
 
-        allQuests.push(...quests);
-    }
+        for(const quest of quests) {
 
-    console.log(allQuests);
+
+            let questData = {
+                title: quest.title,
+                type: quest.type,
+                objectives: quest.objectives,
+                rewards: quest.rewards,
+                kappa: quest.kappa,
+            }
+
+            console.log(`Currently on ${quest.link}`)
+            if(quest.link) {
+                await page.goto(`https://escapefromtarkov.fandom.com/${quest.link}`);
+                await page.waitForSelector('.va-infobox');
+
+                let prev = '';
+                let next = '';
+
+                const pageDetail = await page.evaluate(() => {
+                    const infobox = document.querySelector('.va-infobox tbody');
+                    if(infobox) {
+                        const rows = infobox.querySelector('#va-infobox0-content');
+                        if(rows) {
+                            const tables = rows.querySelectorAll('table');
+                            if(tables) {
+                                const relatedQuests = tables[2].querySelectorAll('tr');
+                                const prevAndNext = relatedQuests[3];
+                                const data = prevAndNext.querySelectorAll('td');
+
+                                if(data.length > 1) {
+                                    prev = data[0].innerText;
+                                    next = data[2].innerText;
+                                    
+                                    return [prev, next];
+                                }
+                                
+                                
+                            }
+                        }
+                    } 
+                })         
+                
+                if(pageDetail) {
+                    [prev, next] = pageDetail;
+                    questData.prev = prev;
+                    questData.next = next;
+
+                    console.log(pageDetail);
+                }
+            }   
+            allQuests.push(questData); 
+            await page.goBack();        
+        }
+    }
 
     await csvWriter.writeRecords(allQuests);
     console.log('CSV file has been written');
